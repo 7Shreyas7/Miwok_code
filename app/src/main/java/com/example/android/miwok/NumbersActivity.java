ef139adfd533ea34a4527194d59ad306f9a3bf20
Shreyas77 <shreyasbanagar7@gmail.com>
2017-08-16 14:38:45 +0530

    Added AudioFocus to Numbers Activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,5 +1,7 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,10 +11,40 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import static android.media.AudioManager.AUDIOFOCUS_LOSS_TRANSIENT;
+
 public class NumbersActivity extends AppCompatActivity {
 
 
     private MediaPlayer mplayer;
+    private AudioManager mAudioManager;
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange)
+                {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS)
+                    {
+                        // Permanent loss of audio focus
+                        // Pause playback immediately
+                        releaseMediaPlayer();
+                    }
+                    else if (focusChange == AUDIOFOCUS_LOSS_TRANSIENT||focusChange==AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK)
+                    {
+                        // Pause playback
+                        mplayer.pause();
+                        mplayer.seekTo(0);
+                    }
+                    else if (focusChange == AudioManager.AUDIOFOCUS_GAIN)
+                    {
+                        // Your app has been granted audio focus again
+                        // Raise volume to normal, restart playback if necessary
+                        mplayer.start();
+                    }
+                }
+            };
+
+
     MediaPlayer.OnCompletionListener mCompletionListener=new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
@@ -44,12 +76,23 @@ public class NumbersActivity extends AppCompatActivity {
         ListView lv = (ListView)findViewById(R.id.list);
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+            {
                 Word currentWord=numbers.get(i);
                 releaseMediaPlayer();
-                mplayer = MediaPlayer.create(NumbersActivity.this,currentWord.getMusicfileId());
-                mplayer.start();
-                mplayer.setOnCompletionListener(mCompletionListener);
+
+                mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+                // Request audio focus for playback
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED)
+                {
+                    // Start playback
+                    mplayer = MediaPlayer.create(NumbersActivity.this, currentWord.getMusicfileId());
+                    mplayer.start();
+                    mplayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
 
@@ -66,6 +109,7 @@ public class NumbersActivity extends AppCompatActivity {
         {
             mplayer.release();
             mplayer=null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
         }
     }
 }
